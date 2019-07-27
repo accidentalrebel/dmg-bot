2b68846d443429202b8d062d0157613353b60da8
accidentalrebel <accidentalrebel@gmail.com>
2019-07-28 00:31:24 +0800

    Made consts.

--- dmgbot.nim
+++ dmgbot.nim
@@ -2,12 +2,17 @@ import nimpy
 import os
 from strutils import replace
 
-var youtubeDlCmd: string = "youtube-dl --write-auto-sub --sub-lang en --skip-download -o ./data/sub " & paramStr(1)
+const outputPath = "./data/sub"
+const outputExt = ".en.vtt"
+
+var youtubeDlCmd: string = "youtube-dl --write-auto-sub --sub-lang en --skip-download -o " & outputPath & ".vtt " & paramStr(1)
+echo "Cmd: " & youtubeDlCmd
+
 discard execShellCmd(youtubeDlCmd)
 
 let webvtt = pyImport("webvtt")
 
-let lines = webvtt.read("./data/sub.en.vtt")
-for i in countup(1, 200, 2):
-  var line: string = lines[i].text.to(string)
-  echo replace(line,"\n","")
+let lines = webvtt.read(outputPath & outputExt)
+# for i in countup(1, 200, 2):
+#   var line: string = lines[i].text.to(string)
+#   echo replace(line,"\n","")
